757609628457ea1bd4dfe6e8e2fc8677aae715c7
hristof <hrsito@hristof.com>
2012-08-25 23:27:52 +0300

    msg

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'rwerwerwerwerwrew';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
